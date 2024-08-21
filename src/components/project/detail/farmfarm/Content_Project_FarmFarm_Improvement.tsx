c3c22425a04d10ecadcd37860d71112135b2eff1
miruy <dbflarla4966@naver.com>
2024-08-21 13:45:36 +0900

    팜팜 이슈 및 헤결방법 추가

--- src/components/project/detail/farmfarm/Content_Project_FarmFarm_Improvement.tsx
+++ src/components/project/detail/farmfarm/Content_Project_FarmFarm_Improvement.tsx
@@ -14,11 +14,11 @@ const Content_Project_FarmFarm_Improvement = () => {
                     <div className="space-y-2">
                         <div className="flex items-center leading-7 font-semibold text-[15px] text-foreground">
                             <IoCloseSharp className="w-6 h-6 fill-red-500 mr-1"/>
-                            모니터링 실패
+                            온/습도 데이터를 시각화하는 과정에서 정의되지 않은 데이터에 접근하여 에러 발생
                         </div>
                         <div className="text-[13px] mx-6 text-zinc-300">
-                            구체적인 거절 이유는 게시자 콘텐츠가 없는 화면에 Google 게재 광고가 포함되었기 때문입니다.
-                            특히, 가치가 낮은 콘텐츠나 아직 준비 중인 화면, 알림 또는 기타 행동 목적의 화면에 광고가 노출되는 것이 문제로 지적되었습니다.
+                            ReChart 라이브러리를 사용하여 온/습도 센서 데이터를 차트로 시각화하는 과정에서,
+                            Y축 값을 설정할 때 적절히 초기화되지 않은 상태의 객체에 접근하려 하여 에러가 발생하였습니다.
                         </div>
                     </div>
 
@@ -31,52 +31,19 @@ const Content_Project_FarmFarm_Improvement = () => {
                             <div className="flex items-center text-zinc-300">
                                 <GoDotFill className="w-3 h-3 mx-1.5"/>
                                 <span className="text-[13px] text-zinc-300">
-                                    Google Ads 정책에 따라 광고가 게재되는 페이지에 유의미한 콘텐츠를 추가하여 가치 있는 페이지로 개선될 수 있도록 하였습니다.
+                                     데이터를 가져오는 과정에서 해당 데이터가 적절히 초기화 될 수 있도록, useEffect() 로직을 추가하여 초기화 과정을 재점검 하였습니다.
                                 </span>
                             </div>
                             <div className="flex items-center text-zinc-300">
                                 <GoDotFill className="w-3 h-3 mx-1.5"/>
                                 <span className="text-[13px] text-zinc-300">
-                                    준비 중인 페이지나 알림 화면에서는 광고를 제거하고, 사용자에게 의미 있는 정보가 포함된 페이지에만 광고를 배치하도록 설정하였습니다.
+                                    데이터 초기화 후 적절히 할당된 데이터를 사용하기 위해 조건부 렌더링을 적용하였으며, 이를 통해 안전한 값을 사용하고 에러를 방지할 수 있었습니다.
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="border-b border-zinc-600 py-5">
-                    <div className="space-y-2">
-                        <div className="flex items-center leading-7 font-semibold text-[15px] text-foreground">
-                            <IoCloseSharp className="w-6 h-6 fill-red-500 mr-1"/>
-                            자동개폐 실패
-                        </div>
-                        <div className="text-[13px] mx-6 text-zinc-300">
-                            구체적인 거절 이유는 게시자 콘텐츠가 없는 화면에 Google 게재 광고가 포함되었기 때문입니다.
-                            특히, 가치가 낮은 콘텐츠나 아직 준비 중인 화면, 알림 또는 기타 행동 목적의 화면에 광고가 노출되는 것이 문제로 지적되었습니다.
-                        </div>
-                    </div>
-
-                    <div className="mt-5 space-y-2">
-                        <div className="flex items-center leading-7 font-semibold text-[15px] text-foreground">
-                            <FaCheck className="w-4 h-4 ml-1 mr-2 fill-green-700"/>
-                            해결방법
-                        </div>
-                        <div className="space-y-2">
-                            <div className="flex items-center text-zinc-300">
-                                <GoDotFill className="w-3 h-3 mx-1.5"/>
-                                <span className="text-[13px] text-zinc-300">
-                                    Google Ads 정책에 따라 광고가 게재되는 페이지에 유의미한 콘텐츠를 추가하여 가치 있는 페이지로 개선될 수 있도록 하였습니다.
-                                </span>
-                            </div>
-                            <div className="flex items-center text-zinc-300">
-                                <GoDotFill className="w-3 h-3 mx-1.5"/>
-                                <span className="text-[13px] text-zinc-300">
-                                    준비 중인 페이지나 알림 화면에서는 광고를 제거하고, 사용자에게 의미 있는 정보가 포함된 페이지에만 광고를 배치하도록 설정하였습니다.
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 {/* 추가 위치 */}
             </div>
         </div>
