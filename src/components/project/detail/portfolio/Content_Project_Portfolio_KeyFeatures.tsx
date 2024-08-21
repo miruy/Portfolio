bd325e7951724a6c5e3728815223b4741f536116
miruy <dbflarla4966@naver.com>
2024-08-21 19:05:14 +0900

    팜팜 서비스 기능 업데이트

--- src/components/project/detail/portfolio/Content_Project_Portfolio_KeyFeatures.tsx
+++ src/components/project/detail/portfolio/Content_Project_Portfolio_KeyFeatures.tsx
@@ -1,5 +1,5 @@
 import {FaCheck} from "react-icons/fa";
-import {GoDotFill} from "react-icons/go";
+import {GoDash, GoDot, GoDotFill} from "react-icons/go";
 
 const Content_Project_Portfolio_KeyFeatures = () => {
     return (
@@ -25,6 +25,16 @@ const Content_Project_Portfolio_KeyFeatures = () => {
                     <div className="flex items-center text-[14px] leading-7 mx-5 py-0.5 text-zinc-300">
                         <GoDotFill className="w-3 h-3 mx-0.5"/>
                         <span className="ml-1.5 text-foreground font-semibold">아래로 이동하는 버튼에 스크롤을 유도하는 시각적 요소를 함께 사용하여 사용자 편의성을 높였습니다.</span>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex items-center leading-7 font-semibold text-[15px] text-foreground">
+                        <FaCheck className="w-4 h-4 mr-1.5"/>
+                        화면 오른쪽 상단에 배치된 GitHub와 개발 블로그 링크
+                    </div>
+                    <div className="text-[13px] mx-6 pb-1 text-zinc-300">
+                        GitHub와 개발 블로그 링크를 화면 오른쪽 상단에 고정 배치하여 접근성을 높였으며, 전체 포트폴리오의 깔끔한 디자인과 조화를 이루도록 아이콘만 표시하였습니다.
                     </div>
                 </div>
 
@@ -84,18 +94,60 @@ const Content_Project_Portfolio_KeyFeatures = () => {
                     <>
                         <div className="flex items-center text-[14px] leading-7 mx-5 py-0.5 text-zinc-300">
                             <GoDotFill className="w-3 h-3 mx-0.5"/>
-                            <span className="ml-1.5 text-foreground font-semibold"></span>
+                            <span className="ml-1.5 text-foreground font-semibold">프로젝트 요약 뷰</span>
                         </div>
                         <div className="text-[13px] mx-6 pb-1 text-zinc-300">
+                            짧은 영상을 썸네일로 게시하여 시각적으로 입체감을 더하고, 반응성을 높여 사용자의 궁금증을 유발할 수 있도록 구성하였습니다.
                         </div>
                     </>
                     <>
                         <div className="flex items-center text-[14px] leading-7 mx-5 py-0.5 text-zinc-300">
                             <GoDotFill className="w-3 h-3 mx-0.5"/>
-                            <span className="ml-1.5 text-foreground font-semibold"></span>
+                            <span className="ml-1.5 text-foreground font-semibold">프로젝트 상세 뷰</span>
                         </div>
-                        <div className="text-[13px] mx-6 pb-1 text-zinc-300">
-                        </div>
+
+                        <>
+                            <div className="text-[13px] mx-6 pb-1 text-foreground font-semibold">
+                                <div className="flex items-center ml-5">
+                                    <GoDot className="mr-1 text-foreground"/>
+                                    <div>상단에 위치한 프로젝트 개요</div>
+                                </div>
+                            </div>
+                            <div className="text-[13px] mx-6 pb-1 text-zinc-300">
+                                <div className="ml-9">
+                                    개발 기간, 개발 인원, 기여도, 프로젝트에 사용된 라이브러리와 툴을 보기 쉽게 표로 정리하여 뷰 상단에 게시하였습니다.
+                                </div>
+                            </div>
+                        </>
+
+                        <>
+                            <div className="text-[13px] mx-6 pb-1 text-foreground font-semibold">
+                                <div className="flex items-center ml-5">
+                                    <GoDot className="mr-1 text-foreground"/>
+                                    <div>작업화면</div>
+                                </div>
+                            </div>
+                            <div className="text-[13px] mx-6 pb-1 text-zinc-300">
+                                <div className="ml-9">
+                                    실제 사용 중인 화면을 이미지로 게시하였고, 이미지 클릭 시 사용자가 직관적으로 이미지를 탐색할 수 있도록 캐러셀 기능을 활용해 큰 화면으로 표시되도록 구현했습니다.
+                                </div>
+                            </div>
+                        </>
+
+                        <>
+                            <div className="text-[13px] mx-6 pb-1 text-foreground font-semibold">
+                                <div className="flex items-center ml-5">
+                                    <GoDot className="mr-1 text-foreground"/>
+                                    <div>작업화면_이미지 크게보기</div>
+                                </div>
+                            </div>
+                            <div className="text-[13px] mx-6 pb-1 text-zinc-300">
+                                <div className="ml-9">
+                                    큰 이미지 탐색 시, 모바일과 웹 환경을 고려하여 캐러셀의 디자인과 이동 방향을 각각 다르게 배치하여 반응형 요소를 적용하였습니다.
+                                </div>
+                            </div>
+                        </>
+
                     </>
                 </div>
 
