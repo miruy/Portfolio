5b1a5726e956b2d75a2efbd2377abef010d3c846
miruy <dbflarla4966@naver.com>
2024-08-26 22:05:06 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
         locale: 'ko_KR',
         images: [
             {
-                url: 'https://api.memocode.dev/memos/1ce6d122-d9a7-49db-9c16-037f61b2e7de/images/4612748f-67cc-41ff-ac9a-f1361c72bab3.png',
+                url: 'https://api.memocode.dev/memos/1ce6d122-d9a7-49db-9c16-037f61b2e7de/images/56f89535-a1a0-4bac-8706-00c6a7173c9b.png',
                 alt: 'portfolio_image',
             },
         ],
@@ -44,7 +44,7 @@ const jsonLd = {
     'url': 'https://yurim-portfolio.vercel.app',
     'image': {
         '@type': 'websiteImage',
-        'url': 'https://api.memocode.dev/memos/1ce6d122-d9a7-49db-9c16-037f61b2e7de/images/4612748f-67cc-41ff-ac9a-f1361c72bab3.png',
+        'url': 'https://api.memocode.dev/memos/1ce6d122-d9a7-49db-9c16-037f61b2e7de/images/56f89535-a1a0-4bac-8706-00c6a7173c9b.png',
         'alt': 'portfolio_image',
     },
 };
