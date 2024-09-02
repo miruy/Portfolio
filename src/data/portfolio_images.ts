2389e2faadcbe8a3917d9a1d78c2300ac0f29745
miruy <dbflarla4966@naver.com>
2024-09-02 17:39:13 +0900

    포트폴리오 이미지 업데이트

--- src/data/portfolio_images.ts
+++ src/data/portfolio_images.ts
@@ -2,15 +2,16 @@ import {Image} from "@/model/image";
 
 const portfolio_images: Image[] = [
     {id: 1, src: "/image/portfolio/portfolio_main.png", title: "메인페이지"},
-    {id: 2, src: "/image/portfolio/portfolio_aboutme1.png", title: "소개_프로필 및 교육 & 경험"},
-    {id: 3, src: "/image/portfolio/portfolio_aboutme2.png", title: "소개_스킬 & 툴"},
-    {id: 4, src: "/image/portfolio/portfolio_project.png", title: "프로젝트_리스트_요약 뷰"},
-    {id: 5, src: "/image/portfolio/portfolio_project_detail1.png", title: "프로젝트_상세 뷰_개요"},
-    {id: 6, src: "/image/portfolio/portfolio_project_detail2.png", title: "프로젝트_상세 뷰_작업화면"},
-    {id: 7, src: "/image/portfolio/portfolio_project_detail3.png", title: "프로젝트_상세 뷰_주요 성과 및 기술적 중점 사항"},
-    {id: 8, src: "/image/portfolio/portfolio_project_detail4.png", title: "프로젝트_상세 뷰_제작동기"},
-    {id: 9, src: "/image/portfolio/portfolio_project_detail5.png", title: "프로젝트_상세 뷰_서비스 기능 및 특징"},
-    {id: 10, src: "/image/portfolio/portfolio_project_detail6.png", title: "프로젝트_상세 뷰_이슈 및 해결방법"},
+    {id: 2, src: "/image/portfolio/portfolio_profile.png", title: "소개_프로필"},
+    {id: 3, src: "/image/portfolio/portfolio_aboutme1.png", title: "소개_교육 & 경험"},
+    {id: 4, src: "/image/portfolio/portfolio_aboutme2.png", title: "소개_스킬 & 툴"},
+    {id: 5, src: "/image/portfolio/portfolio_project.png", title: "프로젝트_리스트_요약 뷰"},
+    {id: 6, src: "/image/portfolio/portfolio_project_detail1.png", title: "프로젝트_상세 뷰_개요"},
+    {id: 7, src: "/image/portfolio/portfolio_project_detail2.png", title: "프로젝트_상세 뷰_작업화면"},
+    {id: 8, src: "/image/portfolio/portfolio_project_detail3.png", title: "프로젝트_상세 뷰_주요 성과 및 기술적 중점 사항"},
+    {id: 9, src: "/image/portfolio/portfolio_project_detail4.png", title: "프로젝트_상세 뷰_제작동기"},
+    {id: 10, src: "/image/portfolio/portfolio_project_detail5.png", title: "프로젝트_상세 뷰_서비스 기능 및 특징"},
+    {id: 11, src: "/image/portfolio/portfolio_project_detail6.png", title: "프로젝트_상세 뷰_이슈 및 해결방법"},
 ];
 
 export default portfolio_images;
